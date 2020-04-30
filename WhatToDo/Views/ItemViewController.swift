73334a0ae9287cd32afd6738c343483f600044cb
Ivan Volokitin <ivolokitin@bk.ru>
2020-04-30 22:25:39 +0300

    Add two round buttons to ItemVC - Next, Do It Update generateItem() method to avoid repeating values

--- WhatToDo/Views/ItemViewController.swift
+++ WhatToDo/Views/ItemViewController.swift
@@ -25,6 +25,33 @@ class ItemViewController: UIViewController {
         return label
     }()
     
+    let nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: 20)
+        button.backgroundColor = .darkGray
+        button.layer.cornerRadius = 35
+        button.contentEdgeInsets = UIEdgeInsets(top: -4, left: 0, bottom: 0, right: 0)
+        button.addTarget(self, action: #selector(nextButton_touchedUpInside), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let doItButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Do it", for: .normal)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 35
+        button.contentEdgeInsets = UIEdgeInsets(top: -4, left: 0, bottom: 0, right: 0)
+        button.addTarget(self, action: #selector(doItButton_touchedUpInside), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    
     // MARK:- Lifecycle
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,17 +86,38 @@ class ItemViewController: UIViewController {
     }
     fileprivate func addViews() {
         view.addSubview(itemLabel)
+        view.addSubview(doItButton)
+        view.addSubview(nextButton)
     }
     
     fileprivate func constrainViews() {
         itemLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
         itemLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
         itemLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        
+        doItButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        doItButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -120).isActive = true
+        doItButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        doItButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        nextButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        nextButton.bottomAnchor.constraint(equalTo: doItButton.topAnchor, constant: -20).isActive = true
+        nextButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        nextButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
     }
     
     // MARK:- Handlers
+    
+    @objc func nextButton_touchedUpInside() {
+        generateItem()
+    }
+    
+    @objc func doItButton_touchedUpInside() {
+        
+    }
+    
     func generateItem() {
-        let item = items.randomElement()
+        let item = items.filter { $0.name != itemLabel.text }.randomElement()
         itemLabel.text = item?.name
     }
 }
