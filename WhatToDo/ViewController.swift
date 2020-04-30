ab6f805c1429a1a56932a90b5330f08620db723a
Ivan Volokitin <ivolokitin@bk.ru>
2020-04-30 19:37:04 +0300

    Initial Commit

---
+++ WhatToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToDo
+//
+//  Created by Ivan Volokitin on 30.04.2020.
+//  Copyright © 2020 Ivan Volokitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
